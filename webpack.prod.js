1ec49e29c5c828314dd3a493cc6ad879694bf2e3
Piotr Hołub <piotrekholub@wp.pl>
2024-04-06 19:00:59 +0200

    Updated webpack production settings

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "source-map",
+  devtool: "hidden-source-map",
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].min.css",
